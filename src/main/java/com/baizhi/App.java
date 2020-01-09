92eae60cc05288aabacfdccc08a39e3bbe0d892e
qlj <710665836@qq.com>
2020-01-09 21:22:20 +0800

    检出后修改上交

--- src/main/java/com/baizhi/App.java
+++ src/main/java/com/baizhi/App.java
@@ -10,6 +10,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRedisHttpSession
 public class App {
     public static void main(String[] args) {
+        System.out.println();
         SpringApplication.run(App.class, args);
     }
 }
